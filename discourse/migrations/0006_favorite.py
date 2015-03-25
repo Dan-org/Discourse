109851753112e103e346940b404603957dd8bd28
Brantley Harris <deadwisdom@gmail.com>
2015-03-24 21:34:26 -0500

    ...

---
+++ discourse/migrations/0006_favorite.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('discourse', '0003_record_tags'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Favorite',
+            fields=[
+                ('subscription_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='discourse.Subscription')),
+            ],
+            options={
+            },
+            bases=('discourse.subscription',),
+        ),
+    ]
