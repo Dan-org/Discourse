44107737b14e1d0cfa46336de50b3f8e2ebb8bbe
Brantley Harris <deadwisdom@gmail.com>
2015-04-19 20:03:30 -0500

    migrations

--- discourse/migrations/0005_auto_20150419_1937.py
+++ discourse/migrations/0005_auto_20150419_1937.py
@@ -6,6 +6,7 @@ from django.conf import settings
 import uuidfield.fields
 import yamlfield.fields
 
+from uuid import uuid4
 
 class Migration(migrations.Migration):
 
@@ -23,50 +24,66 @@ class Migration(migrations.Migration):
                 ('tags', models.TextField(blank=True)),
                 ('keys', models.TextField(blank=True)),
                 ('publish_keys', models.TextField(blank=True)),
-                ('owner', models.ForeignKey(related_name=b'channels', blank=True, to=settings.AUTH_USER_MODEL, null=True)),
                 ('created', models.DateTimeField(auto_now_add=True)),
+                ('owner', models.ForeignKey(related_name=b'channels', blank=True, to=settings.AUTH_USER_MODEL, null=True)),
             ],
             options={
             },
             bases=(models.Model,),
         ),
+        #migrations.CreateModel(
+        #    name='Favorite',
+        #    fields=[
+        #        ('subscription_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='discourse.Subscription')),
+        #    ],
+        #    options={
+        #    },
+        #    bases=('discourse.subscription',),
+        #),
         migrations.CreateModel(
             name='Message',
             fields=[
-                ('uuid', uuidfield.fields.UUIDField(primary_key=True, auto=True, serialize=False)),
+                ('uuid', uuidfield.fields.UUIDField(serialize=False, primary_key=True)),
                 ('type', models.SlugField(max_length=255)),
-                ('channel', models.ForeignKey(related_name=b'messages', to='discourse.Channel')),
                 ('order', models.IntegerField(default=0)),
+                ('depth', models.IntegerField(default=0)),
                 ('created', models.DateTimeField(auto_now_add=True)),
                 ('modified', models.DateTimeField(auto_now=True)),
                 ('deleted', models.DateTimeField(null=True, blank=True)),
-                ('tags', models.CharField(max_length=255, null=True, blank=True)),
-                ('keys', models.CharField(max_length=255, null=True, blank=True)),
+                ('tags', models.TextField(null=True, blank=True)),
+                ('keys', models.TextField(null=True, blank=True)),
                 ('content', yamlfield.fields.YAMLField(null=True, blank=True)),
                 ('value', models.IntegerField(default=0)),
                 ('author', models.ForeignKey(related_name=b'messages', blank=True, to=settings.AUTH_USER_MODEL, null=True)),
+                ('channel', models.ForeignKey(related_name=b'messages', to='discourse.Channel')),
                 ('parent', models.ForeignKey(related_name=b'children', blank=True, to='discourse.Message', null=True)),
             ],
             options={
-                'ordering': ['parent_id', 'order', '-created']
+                'ordering': ['depth', 'parent_id', 'order', '-created'],
             },
             bases=(models.Model,),
         ),
-        migrations.RenameModel(
-            old_name='attachment',
-            new_name='old_attachments',
+        migrations.RemoveField(
+            model_name='comment',
+            name='author',
         ),
-        migrations.CreateModel(
-            name='Attachment',
-            fields=[
-                ('uuid', uuidfield.fields.UUIDField(auto=True, primary_key=True)),
-                ('mimetype', models.CharField(max_length=255)),
-                ('filename', models.CharField(max_length=255)),
-                ('source', models.FileField(upload_to=b'attachments')),
-                ('message', models.ForeignKey(related_name=b'attachments', to='discourse.Message')),
-            ],
-            options={
-            },
-            bases=(models.Model,),
+        migrations.RemoveField(
+            model_name='comment',
+            name='parent',
+        ),
+        migrations.RemoveField(
+            model_name='vote',
+            name='user',
+        ),
+        migrations.RemoveField(
+            model_name='attachment',
+            name='author',
+        ),
+        migrations.RemoveField(
+            model_name='attachment',
+            name='id',
+        ),
+        migrations.DeleteModel(
+            name='attachment'
         )
     ]
